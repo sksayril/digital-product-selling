bddfe1d33636cdabfc51e385a2c5f8e320c124a3
sksayril <sksayril123@gmail.com>
2025-05-19 15:52:06 +0530

    :gensum

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "~/styles/globals.css";
 
 import { type Metadata } from "next";
-import { Geist } from "next/font/google";
+import { GeistSans } from "geist/font";
 
 import { TRPCReactProvider } from "~/trpc/react";
 import { Navbar } from "~/components/Navbar";
@@ -33,16 +33,13 @@ export const metadata: Metadata = {
   },
 };
 
-const geist = Geist({
-  subsets: ["latin"],
-  variable: "--font-geist-sans",
-});
+// GeistSans already has variable property, no need to call it as a function
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${geist.variable}`}>
+    <html lang="en" className={`${GeistSans.variable}`}>
       <body className="min-h-screen flex flex-col">
         <TRPCReactProvider>
           <Navbar />
